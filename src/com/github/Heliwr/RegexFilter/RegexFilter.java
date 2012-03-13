b048439c914777f9cb67290b0b9d54df7568f057
Richard Luther <heliwr@gmail.com>
2012-03-13 11:39:28 -0700

    first commit

--- src/com/github/Heliwr/RegexFilter/RegexFilter.java
+++ src/com/github/Heliwr/RegexFilter/RegexFilter.java
@@ -10,11 +10,7 @@ import java.util.regex.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.Server;
-import org.bukkit.event.Event;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
 
